c8db29a40bea8a869bfd0df5ac08d1331b2f86dd
topanok <topanok2015@gmail.com>
2020-03-02 17:14:55 +0200

    Add new files

---
+++ framework/IRequest.php
@@ -0,0 +1,10 @@
+<?php
+	namespace Framework;
+	
+	interface IRequest {
+		public function getParams(): array;
+		public function getQueryParams(): array ;
+		public function getParam(string $param): string ;
+		public function getQueryParam(string $query): string ;
+	}
+?>
---
+++ framework/Request.php
@@ -0,0 +1,20 @@
+<?php
+	namespace Framework;
+	
+	use IRequest;
+	
+	class Request implements IRequest{
+		public function getParams(): array{
+			return $_POST;
+		}
+		public function getQueryParams(): array {
+			return $_GET;
+		}
+		public function getParam(string $param): string {
+			return $_POST[$param];
+		}
+		public function getQueryParam(string $query): string {
+			return $_GET[$query];
+		}
+	}
+?>
