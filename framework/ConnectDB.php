4c7f6d12021a0c6e1af205e3c4ff0fbf32d6e5ab
topanok <topanok2015@gmail.com>
2020-03-17 15:01:04 +0200

    Change

--- framework/ConnectDB.php
+++ framework/ConnectDB.php
@@ -4,7 +4,7 @@
 	use App\Config\DBConfig;
 	use PDO;
 	
-	class ConnectDB extends Config{
+	class ConnectDB extends DBConfig{
 		private $dsn = "mysql:host=localhost;dbname=test;charset=utf8";
 		private $options = [
 			  PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION,
