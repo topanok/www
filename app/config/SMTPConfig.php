26e9499f35f5e6c3d87b619d39f34d1757b034e2
topanok <topanok2015@gmail.com>
2020-03-20 13:30:45 +0200

    Change

--- app/config/SMTPConfig.php
+++ app/config/SMTPConfig.php
@@ -2,18 +2,18 @@
 	namespace App\Config;
 	
 	abstract class SMTPConfig {
-		public $charSet 	= "UTF-8";             
-		public $SMTPAuth   	= true;
-		public $isHtml		= true;
-		public $host      	= 'smtp.ukr.net';
-		public $userName    = 'topanok2015@ukr.net';
-		public $password    = 'sEw0l4cgNlNLAw0I';
-		public $SMTPSecure  = 'ssl';
-		public $port        = 465;
-		public $fromMail	='topanok2015@ukr.net';
-		public $fromName	='Bogdan';
-		public $sendCopy	=true;
-		public $sendCopyMail='topanok2015@ukr.net';
-		public $sendCopyName='Bogdan';
+		public $charSet 		= "UTF-8";             
+		public $SMTPAuth   		= true;
+		public $isHtml			= true;
+		public $host      		= 'smtp.ukr.net';
+		public $userName   		= 'topanok2015@ukr.net';
+		public $password    	= 'sEw0l4cgNlNLAw0I';
+		public $SMTPSecure  	= 'ssl';
+		public $port        	= 465;
+		public $fromMail		= 'topanok2015@ukr.net';
+		public $fromName		= 'Bogdan';
+		public $sendCopy		= true;
+		public $sendCopyMail	= 'topanok2015@ukr.net';
+		public $sendCopyName	= 'Bogdan';
 	}
 ?>
