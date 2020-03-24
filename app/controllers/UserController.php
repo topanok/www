f9a0cd95874521ef18ae9d2219e0179adc634f4b
topanok <topanok2015@gmail.com>
2020-03-24 14:27:49 +0200

    add action 'login' to UserController

--- app/controllers/UserController.php
+++ app/controllers/UserController.php
@@ -7,24 +7,7 @@
 	use Framework\Db;
 	
 	class UserController extends Controller{
-		private $form;
-		public function register(){
-			$form = new FormBuilder;
-			$form->setId('Reg');
-			$form->setMethod('POST');
-			$form->setClass('form-control');
-			
-			$form->addField('text',['name'=>'name','placeholder'=>'Ім\'я']);
-			$form->addField('text',['name'=>'surname','placeholder'=>'Фамілія']);
-			$form->addField('email',['name'=>'email','placeholder'=>'Email']);
-			$form->addField('text',['name'=>'login','placeholder'=>'Login']);
-			$form->addField('password',['maxlength'=>'25', 'name'=>'password', 'placeholder'=>'Пароль']);
-			$form->addField('submit',['value'=>'Відправити']);
-			$data=$form->createForm();
-			
-			$this->render('app/views/ViewRegister.php',$data);
-			$this->save();
-		}
+
 		private function save(){	
 			$request=$this->getRequest();
 			$data=$request->getParams();
@@ -45,6 +28,59 @@
 			else{
 				echo 'Заповніть всі поля!';
 			} 
+		}
+
+		public function register(){
+			$form = new FormBuilder;
+			$form->setId('Reg');
+			$form->setMethod('POST');
+			$form->setClass('form-control');
+			
+			$form->addField('text',['name'=>'name','placeholder'=>'Ім\'я']);
+			$form->addField('text',['name'=>'surname','placeholder'=>'Фамілія']);
+			$form->addField('email',['name'=>'email','placeholder'=>'Email']);
+			$form->addField('text',['name'=>'login','placeholder'=>'Login']);
+			$form->addField('password',['maxlength'=>'25', 'name'=>'password', 'placeholder'=>'Пароль']);
+			$form->addField('submit',['value'=>'Відправити']);
+			$data=$form->createForm();
+			
+			$this->render('app/views/ViewRegister.php',$data);
+			$this->save();
+		}
+
+		public function login(){
+			session_start();
+			$form = new FormBuilder;
+			$form->setId('Reg');
+			$form->setMethod('POST');
+			$form->setClass('form-control');
+			
+			$form->addField('text',['name'=>'login','placeholder'=>'Логін']);
+			$form->addField('password',['maxlength'=>'25', 'name'=>'password', 'placeholder'=>'Пароль']);
+			$form->addField('submit',['value'=>'Відправити']);
+			$data=$form->createForm();
+			
+			$this->render('app/views/ViewLogin.php',$data);
+
+			$request=$this->getRequest();
+			$data=$request->getParams();
+			$user=new UsersModelRepository;
+			$db=new DB($user->getTable());
+			if(isset($data['login']) && isset($data['password'])){
+				$userIsset=$db->getByParam('login',$data['login']);
+				if(!empty($userIsset)){
+					if(password_verify ( $data['password'] , $userIsset[0]['password'] ) ) {
+						$_SESSION['login']=$data['login'];
+						echo 'Вітаємо '.$userIsset[0]['name'].' ! Ви увійшли як '.$data['login'].'.';
+					}
+					else{
+						echo 'Невірний пароль!';
+					}
+				}
+				else{
+					echo 'Користувача з таким логіном не існує';
+				}
+			}
 		}
 	}
 ?>
