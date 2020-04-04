1cc91d07b1ebbc27e8b6b00733d1da7772bd3a2b
yuriy_bodak <yuriy.bodak@nordicwebteam.se>
2020-04-04 15:03:57 +0300

    Add file uploader interface

---
+++ framework/Filesystem/Api/FileUploaderInterface.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace Framework\Filesystem\Api;
+
+interface FileUploaderInterface
+{
+    /** Set file from request */
+    public function setFile($file): void;
+
+    /** Path where upload file */
+    public function setDir(string $path): void;
+
+    /** Add allow formats for uploading files */
+    public function addAllowedFormats(array $allowedFormats): void;
+
+    /** Get size for uploading files in MB */
+    function getSize(): int;
+
+    /** Set max size for uploading files in MB */
+    function setMaxSize(int $size): void;
+
+    /** Return file mime type, for example image/jpeg */
+    function getMimeType(int $size): string;
+
+    /** Upload file to the filesystem */
+    function uploadFile(): bool;
+}
