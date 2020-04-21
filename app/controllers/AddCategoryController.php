33d427bdd0ccb637e942fb1bbe82cef1ca4643e2
topanok <topanok2015@gmail.com>
2020-04-21 22:11:06 +0300

    change

--- app/controllers/AddCategoryController.php
+++ app/controllers/AddCategoryController.php
@@ -8,6 +8,7 @@
 
 	class AddCategoryController extends Controller{
 		private $idForm='addCat';
+		private $catsName=[];
 		public function add(){
 			$form = new FormBuilder;
 			$form->setAction('http://localhost/addcategory/save');
@@ -16,7 +17,7 @@
 			$form->setClass('');
 			
 			$form->addField('text',['name'=>'newCat','class'=>'form-control','placeholder'=>'Нова категорія']);
-			$form->addField('select',['name'=>'parentCat','class'=>'form-control','option_values'=>$this->getCategories()]);
+			$form->addField('select',['name'=>'parentCat','class'=>'form-control', 'title'=>'Виберіть до якої категорії належить нова категорія', 'option_values'=>$this->getCategories()]);
 			$form->addField('submit',['name'=>'submit', 'value' =>'Додати','class'=>'btn btn-primary btn-lg btn-block']);
 
 			$data=$form->createForm();
@@ -30,25 +31,25 @@
 			$catModel=new CategoryModelRepository;
 			$categories=$catModel->getItems();
 			$catsName=[];
+			$catsName[0]='Головна';
 			for ($i=0; $i<count($categories); $i++) {
 				$catsName[]=$categories[$i]->getName();
 			}
+			$this->catsName=$catsName;
 			return $catsName;
 		}
 
 		public function save(){
-			$categories=$this->getCategories();
 			$request = new Request;
 			$post = $request->getParams();
 			unset($post['submit']);
+			$catsName=$this->getCategories();
 			if(empty($post['newCat'])){
 				$_SESSION['errors']['form'][$this->idForm]['newCat']['empty_fields']='<pre style="background-color: #F6CEEC">Заповніть це поле!  ↑↑↑</pre>';
 			}
-			else{
-				foreach($categories as $value){
-					if($value==$post['newCat']){
-						$_SESSION['errors']['form'][$this->idForm]['newCat']='<pre style="background-color: #F6CEEC">Вже існує така категорія!  ↑↑↑</pre>';
-					}
+			foreach($catsName as $value){
+				if($value==$post['newCat']){
+					$_SESSION['errors']['form'][$this->idForm]['newCat']='<pre style="background-color: #F6CEEC">Вже існує така категорія!  ↑↑↑</pre>';
 				}
 			}
 			if (!empty($_SESSION['errors']['form'][$this->idForm])) {
@@ -59,7 +60,12 @@
 				$objDb=$catModel->getObjDb($catModel->getTable());
 				$data=$objDb->getByParam('name',$post['parentCat']);
 				$arr=[];
-				$arr['parent_id']=$data[0]['id'];
+				if($post['parentCat']=='Головна'){
+					$arr['parent_id']=0;
+				}
+				else{
+					$arr['parent_id']=$data[0]['id'];
+				}
 				$arr['name']=$post['newCat'];
 				$objDb->save($catModel->set($arr));
 				echo '<h1>Катерорія успішно створена!</h1>';
